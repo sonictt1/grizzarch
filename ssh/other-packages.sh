6ceb790843a4fe453129ff42d970b3bab3c79af9
Nic <nic@Grizzly>
2020-09-23 15:36:42 -0500

    removed auto xrandr now that vboxservice is working, also added more default software

--- ssh/other-packages.sh
+++ ssh/other-packages.sh
@@ -1,4 +1,5 @@
 pacman -S --noconfirm xorg xorg-xinit xterm xcompmgr
 pacman -S --noconfirm ttf-droid
-pacman -S --noconfirm i3-gaps dmenu
-pacman -S --noconfirm git vim termite firefox
+pacman -S --noconfirm i3-gaps dmenu i3status
+pacman -S --noconfirm git vim termite
+pacman -S --noconfirm --needed base-devel
